7a1c5ed5bd207502b09e7cbe7b1a238a6e8b74db
Anabel Casero <127798361+AnnCasero@users.noreply.github.com>
2024-06-26 13:36:35 -0300

    Update views.router.js

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = Router(); 
 
-router.get("/", async (req, res) => {
+router.get("/realtimeproducts", async (req, res) => {
     res.render("realtimeproducts");
 })
 
