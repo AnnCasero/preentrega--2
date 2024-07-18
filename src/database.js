f5a48aac15bf8f92f4cc0c01698901e396766128
AnnCasero <anabelcasero19@gmail.com>
2024-07-18 00:08:11 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://anabelcasero19:papitas12345@cluster0.xpxmn4w.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb+srv://anabelcasero19:coderhouse@cluster0.xpxmn4w.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0")
 .then( () => console.log("Conexion exitosa"))
 .catch((error)=> console.log("Ha ocurrido un error", error)) 
